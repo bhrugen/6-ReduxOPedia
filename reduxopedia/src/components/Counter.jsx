8c7fc4fd55b85695f10a23478973452884da2491
bhrugen <bhrugen1990@gmail.com>
2025-07-12 12:48:34 -0500

    Section 10 - UseSelector Hook

---
+++ reduxopedia/src/components/Counter.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+
+function Counter() {
+  const count = useSelector((state) => state.counterStore.count);
+
+  return <div>Counter {count}</div>;
+}
+
+export default Counter;
