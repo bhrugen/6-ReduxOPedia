c5be62edf8e5321b1ac1d996e9d84f3844d4ac02
bhrugen <bhrugen1990@gmail.com>
2025-07-12 14:22:40 -0500

    Section 10 - Assignment 15 - Reset App

---
+++ reduxopedia/src/components/ResetApp.jsx
@@ -0,0 +1,9 @@
+function ResetApp() {
+  return (
+    <div className="text-center pt-4">
+      <button className="btn btn-warning">Reset App</button>
+    </div>
+  );
+}
+
+export default ResetApp;
