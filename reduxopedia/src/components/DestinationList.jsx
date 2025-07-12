dff9f9891c12037fa1ddd15c624273367d54f5f2
bhrugen <bhrugen1990@gmail.com>
2025-07-12 13:50:06 -0500

    Section 10 - Assignment 14 Destination List

---
+++ reduxopedia/src/components/DestinationList.jsx
@@ -0,0 +1,12 @@
+function DestinationList() {
+  return (
+    <div className="text-center row border-bottom">
+      <div className="col-8 pt-2">Name</div>
+      <div className="col-4">
+        <button className="btn btn-success form-control m-1">Details</button>
+      </div>
+    </div>
+  );
+}
+
+export default DestinationList;
